a4cb5adbb78d0fab1d8c602d90ddefcc932a9c83
Squadelho <81372361+Squadelho@users.noreply.github.com>
2024-03-03 19:37:15 +0000

    Initial comit

--- dot_config/powershell/user_profile.ps1
+++ dot_config/powershell/user_profile.ps1
@@ -1,7 +1,30 @@
+# Set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Oh-My-Posh
 oh-my-posh init pwsh --config 'C:\Users\HJMO-\.config\powershell\squadelho.omp.json' | Invoke-Expression
 oh-my-posh init pwsh | Invoke-Expression
 
+# PSReadLine
+Set-PsReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# ALIAS
+Set-Alias -Name vim -Value Nvim
+Set-Alias g git
+Set-Alias grep findstr
+
+# PATHS
+$ChezPath = "C:\Users\HJMO-\.local\share\chezmoi"
+$settings = (Get-Item "C:\users\$env:UserName\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState\settings.json")
+
+# FUNCTIONS
+function ll {Get-ChildItem}
+function la {Get-ChildItem -fo}
 if ($host.Name -eq 'ConsoleHost')
 {
-    Import-Module PSReadLine
+	function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+    	Set-Alias -Name ls -Value ls_git -Option Private
 }
